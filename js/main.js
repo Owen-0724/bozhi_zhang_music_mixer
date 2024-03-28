76dd070296593e23e89615dd38d6082c9d5c489e
Owen-0724 <144960357+Owen-0724@users.noreply.github.com>
2024-03-28 01:29:01 -0400

    add audio function

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 let 
-	
 	puzzleBoard = document.querySelector(".soundtracks"),
 	puzzlePieces = document.querySelectorAll(".simple_set img"),
 	dropZones = document.querySelectorAll('.drop_zone'),
 	
 	draggedPiece,
 
-    resetBtn = document.querySelector('.reset_btn');
+    resetBtn = document.querySelector('#reset_btn');
+
+const
+    albumCovers = document.querySelectorAll("#album-art img"),
+    theAudioEl = document.querySelector('#audioEl'),
+    playButton = document.querySelector('#play_btn'),
+    pauseButton = document.querySelector('#pause_btn'),
+    rewindButton = document.querySelector('#restart_btn'),
+    volSlider = document.querySelector('#volume_control');
 
 const originalParents = {};
 
@@ -22,24 +29,18 @@ function reset() {
 }
 function handleStartDrag() { 
 	console.log('started dragging this piece:', this);
-	// store a reference to the puzzle piece image that we're dragging
-	// so we can use it later and move it to a drop zone
 	draggedPiece = this;
 }
 
 function handleDragOver(e) { 
-	e.preventDefault(); // e is shorthand for event
-	// this overrides the default dragover behaviour
+	e.preventDefault();
 	console.log('dragged over me'); 
 }
 
 function handleDrop(e) { 
 	e.preventDefault();
 	console.log('dropped something on me');
-	// bug fix #1 should go here, and it's at most 3 lines of JS code
-
-	// this line is going to move the dragged piece from the left side of the board
-	// into whatever drop zone we choose. appendChild means "add element to the container"
+	
 	if (this.childElementCount === 0) {
 		this.appendChild(draggedPiece);
 	} else {
@@ -47,24 +48,18 @@ function handleDrop(e) {
 	};
 }
 
+function playSelectedTracks() {
+    let selectedTracks = Array.from(document.querySelectorAll('input[type=checkbox]:checked')).map(checkbox => checkbox.value);
+  
+    selectedTracks.forEach(track => {
+      let audio = new Audio(track);
+      audio.play();
+    });
+  }
 
-
-// step 2
-// event handling always goes at the bottom => 
-// how do we want users to interact with our app
-
-// 1 to 1 event handling
-//theButton.addEventListener("click", changeBGImage);
-
-// 1 to many event handling
-
-// add the drag event handling to the puzzle pieces
 puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
-
-// add the dragover AND the drop event handling to the drop zones
 dropZones.forEach(zone => zone.addEventListener("dragover", handleDragOver));
-
-// add the drop event handling
 dropZones.forEach(zone => zone.addEventListener("drop", handleDrop));
 
 resetBtn.addEventListener("click",reset);
+playButton.addEventListener("click",playSelectedTracks);
